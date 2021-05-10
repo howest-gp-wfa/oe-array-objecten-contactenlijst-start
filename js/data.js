e6acdbf818c8bffc2119aa65a58c248f859ac128
maximlesy <maximlesy@gmail.com>
2021-05-10 19:56:33 +0200

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,35 @@
+var contacts = [
+    {
+        name: "Maxim Lesy",
+        telephone: "050 10 20 30",
+        email: "maxim.lesy@howest.be",
+        address: {
+            street: "Rijselstraat",
+            number: 1,
+            postalCode: 8000,
+            city: "Brugge"
+        }
+    },
+    {
+        name: "Bart Soete",
+        telephone: "09 040 50 60",
+        email: "bart.soete@howest.be",
+        address: {
+            street: "Kerkstraat",
+            number: 9,
+            postalCode: 9000,
+            city: "Gent"
+        }
+    },
+    {
+        name: "Mileto Di Parco",
+        telephone: "050 90 80 70",
+        email: "mileto.di.marco@howest.be",
+        address: {
+            street: "Torendreef",
+            number: 15,
+            postalCode: 1000,
+            city: "Brussel"
+        }
+    }
+];
